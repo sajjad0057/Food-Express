cc82515137fd769bfd6328c00e5f094967de9dfd
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-11-22 00:07:01 +0600

    adding forms(controll with state) and bind function in state

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -3,17 +3,22 @@ import Menu from './Menu.jsx';
 import Home from './Home.jsx';
 import Contact from './Contact.jsx';
 import About from './About.jsx';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 
 
 const Body = () => {
   return (
     <div>
-        <Route path="/" exact component={Home}/>
+      <Switch>
+        <Route path="/home" exact component={Home}/>
         <Route path="/menu" exact component={Menu}/>
         <Route path="/contact" exact component={Contact}/>
         <Route path="/about" exact component={About}/>
+        <Redirect from="/" to="/home" />
+
+      </Switch>
+
     </div>
   );
 };
--- src/components/body/Contact.jsx
+++ src/components/body/Contact.jsx
@@ -1,13 +1,160 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, Col } from "reactstrap";
 
 class Contact extends Component {
-    render(){
-        return(
-            <div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: "",
+      agree: "",
+      contactType: "Tel.",
+      message: "",
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-            </div>
-        )
-    }
+  handleInputChange = event =>{
+      const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+      const name = event.target.name;
+      this.setState({
+          [name]:value,
+      })
+  }
+
+  handleSubmit = e=>{
+      console.log(this.state);
+      e.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row row-content" style={{paddingLeft:"20px", textAlign:"left"}}>
+          <div className="col-12 col-md-7">
+            <hr />
+            <h3>Send Us Your Feedback</h3>
+            <hr />
+          </div>
+          <div className="col-12">
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup row>
+                <Label htmlFor="firstname" md={2}>
+                  First Name :
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    name="firstname"
+                    placeholder="First Name"
+                    value={this.state.firstname}
+                    onChange = {this.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="lastname" md={2}>
+                  Last Name :
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    name="lastname"
+                    placeholder="Last Name"
+                    value={this.state.lastname}
+                    onChange = {this.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="telnum" md={2}>
+                  Telephone :
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="tel"
+                    name="telnum"
+                    placeholder="Tel. No"
+                    value={this.state.telnum}
+                    onChange = {this.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="email" md={2}>
+                  Email :
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange = {this.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 6, offset: 2 }}>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="checkbox"
+                        name="agree"
+                        checked={this.state.agree}
+                        onChange = {this.handleInputChange}
+                      />
+                      <strong>May we contact you ?</strong>
+                    </Label>
+                  </FormGroup>
+                </Col>
+                <Col md={{ size: 3, offset: 1 }}>
+                  <Input
+                    type="select"
+                    name="contactType"
+                    value={this.state.contactType}
+                    onChange = {this.handleInputChange}
+                  >
+                    <option> Tel.</option>
+                    <option> Email</option>
+                  </Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="message" md={2}>
+                  Your Feedback :
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="textarea"
+                    name="message"
+                    value={this.state.message}
+                    row="12"
+                    onChange = {this.handleInputChange}
+                  ></Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                  <Col md={{size : 10, offset:2}}>
+                      <div style={{ textAlign:"right"}}>
+                      <Button type="submit" color="secondary" > 
+                        Send Feedback
+                      </Button>
+
+                      </div>
+
+                  </Col>
+              </FormGroup>
+            </Form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Contact;
