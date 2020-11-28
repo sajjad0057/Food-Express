fdd732ce22030fce4175c4f0aea35a7b0f561dfa
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-11-29 03:19:31 +0600

    Applying Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Reducer } from './reducer.js';
+import logger from 'redux-logger';
+
+/* logger is a opensource Middleware package,
+ * logger basically monitor each step and print step in console when dispatching action to Store.
+ * For this we passing logger as parameter with applyMiddleware().
+ * applyMiddleware( ) receives all middleware as parameter.
+ */
 
 
-const myStore = createStore(Reducer);
+const myStore = createStore(Reducer,applyMiddleware(logger));
 
 export default myStore;
